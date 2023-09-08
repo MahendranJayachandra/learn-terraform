3750e62151b61a85494d19f7786702b0199e7bb3
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-08 14:31:27 +0530

    Added variables

---
+++ 03-vars/vars.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  default = "Muthulakshmi"
+}
+
+variable "age" {
+  default = 30
+}
+
+variable "female" {
+  default = yes
+}
+
+output "Name" {
+  value = "Name is ${var.name}"
+}
+
+output "age" {
+  value = "age is ${var.age}"
+}
+
+output "female" {
+  value = "Female = ${female}"
+}
