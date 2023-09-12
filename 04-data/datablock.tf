fbd708bbf0409cf1908b7dcff2c7f8d3201ca485
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-12 22:30:24 +0530

    created another data source to get spot price

--- 04-data/datablock.tf
+++ 04-data/datablock.tf
@@ -14,31 +14,11 @@ output "example" {
   value = data.aws_ami.example.id
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+data "aws_ec2_spot_price" "getspotprice" {
+  availabilityZone = "us-east-1"
+  instanceType = "t3.medium"
 }
 
 output "price" {
-  value = resource.aws_instance.web.price
+  value = data.aws_ec2_spot_price.getspotprice.spot_price
 }
