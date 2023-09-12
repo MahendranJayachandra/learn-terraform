9b7ecea8823351ceb1b2928c342e99d79a7425d2
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-12 12:47:08 +0530

    added square bracket for owners

--- 04-data/datablock.tf
+++ 04-data/datablock.tf
@@ -2,7 +2,7 @@ data "aws_ami" "example" {
   #executable_users = "322657627157"
   most_recent      = true
   name_regex       = "WorkstationImage"
-  owners           = "322657627157"
+  owners           = ["322657627157"]
 }
 
 # Configure the AWS Provider
