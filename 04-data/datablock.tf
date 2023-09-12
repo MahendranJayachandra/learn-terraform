5acab52a34fa3ce096adbcf30c9565d8ba9a13a1
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-12 13:36:29 +0530

    Included aws image price

--- 04-data/datablock.tf
+++ 04-data/datablock.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "example" {
-  #executable_users = "322657627157"
+  #executable_users = "322657627157" # the program fetching zero results when I gave my account id as executable_users
   most_recent      = true
   name_regex       = "WorkstationImage"
   owners           = ["322657627157"]
@@ -12,4 +12,33 @@ provider "aws" {
 
 output "example" {
   value = data.aws_ami.example.id
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "price" {
+  value = data.aws_ami.ubuntu.price
 }
