61b484c827fd03f37377f43a401e83d2e2dc54d7
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-12 09:12:22 +0530

    modified data

--- 04-data/datablock.tf
+++ 04-data/datablock.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "example" {
   executable_users = ["322657627157"]
   most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
+  name_regex       = "devops-practice"
   owners           = ["322657627157"]
 }
