a3bc60e0d74922069191028543581c65e46a3277
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-12 09:25:28 +0530

    provided my own created AMI

--- 04-data/datablock.tf
+++ 04-data/datablock.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "example" {
   executable_users = ["322657627157"]
   most_recent      = true
-  name_regex       = "devops-practice"
+  name_regex       = "WorkstationImage"
   owners           = ["322657627157"]
 }
