ef9e083798ee6612e653759a54177ba9b049a49d
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-18 21:36:57 +0530

    modified account id in data block

--- 05-resource/resource.tf
+++ 05-resource/resource.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "web" {
 }
 
 data "aws_ami" "example" {
-  executable_users = ["self"]
+  executable_users = [322657627157]
   most_recent      = true
   name_regex       = "WorkstationImage"
-  owners           = ["self"]
+  owners           = [322657627157]
 }
