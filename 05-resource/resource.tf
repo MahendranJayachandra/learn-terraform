86312139eb3b3a81c63ceda10fc96a3a942debed
mahendran jayachandra <mahendran_res@yahoo.com>
2023-09-26 17:15:25 +0530

    Moved provisioner inside resource block

--- 05-resource/resource.tf
+++ 05-resource/resource.tf
@@ -2,16 +2,8 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t2.micro"
   security_groups = ["sg-0c8c5601fb85d7a11"]
-}
 
-data "aws_ami" "example" {
-  # executable_users = [322657627157]
-  most_recent      = true
-  name_regex       = "WorkstationImage"
-  owners           = [322657627157]
-}
-
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     connection {
     type     = "ssh"
     user     = "centos"
@@ -24,3 +16,11 @@ provisioner "remote-exec" {
       "ansible-pull localhost, -i https://github.com/MahendranJayachandra/roboshop-ansible main.yml -e role_name=frontend",
     ]
   }
+}
+
+data "aws_ami" "example" {
+  # executable_users = [322657627157]
+  most_recent      = true
+  name_regex       = "WorkstationImage"
+  owners           = [322657627157]
+}
