b6d738f7ee15d2ac4754d0b2b47105ce1638a47c
mahendran jayachandra <mahendran_res@yahoo.com>
2023-10-10 13:24:24 +0530

    functions

--- 09-functions/main.tf
+++ 09-functions/main.tf
@@ -6,4 +6,22 @@ output "courses" {
   value = "course name is ${var.courses[1]}"
 }
 
+variable "person" {
+  default = {
+    mahendran = {
+      age = 35
+      gender = male
+    },
+    muthulakshmi = {
+      age = 30
+      gender = female
+    }
+  }
+  
+}
+
+output "person" {
+  value = "course to choose ${element(var.courses,1}"
+}
+
  
